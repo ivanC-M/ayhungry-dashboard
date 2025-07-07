152528c136aa124dbfe9a082ccea4325d6ed301c
ivanC-M <ivan@ayhungry.com>
2025-07-07 15:44:12 -0400

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/ayhungry-dashboard',
+  assetPrefix: '/ayhungry-dashboard',
+};
+export default nextConfig;
